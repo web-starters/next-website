e8601b25163200f7ce514543f126a253cd1bac19
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-06-20 00:58:56 +0200

    removed js-cookie. Changed locale management

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,23 +1,17 @@
-import Cookies from 'js-cookie';
 import { usePathname, useRouter } from 'next/navigation';
 import { i18n, type Locale } from '@/i18n-config';
 
 export const useLocale = () => {
   const router = useRouter();
   const pathname = usePathname();
-  const locale = pathname.split('/')[1];
-  const asPath = pathname.replace('/' + locale, '');
-  const { locales } = i18n;
+  const segments = pathname.split('/');
 
-  const handleLocale = (value: Locale) => {
-    if (!locales.includes(value)) return;
+  const handleLocale = (locale: Locale) => {
+    if (!i18n.locales.includes(locale)) return;
 
-    Cookies.set('NEXT_LOCALE', value, { expires: 30, secure: true });
-
-    void router.replace('/' + value + asPath);
+    segments[1] = locale;
+    void router.push(segments.join('/'));
   };
 
-  const localeOptions = locales.map(elem => ({ name: elem, value: elem }));
-
-  return { locale, handleLocale, localeOptions };
+  return { locale: segments[1], handleLocale };
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
 import { NextResponse, type NextRequest } from 'next/server';
+import { match as matchLocale } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
 import { i18n } from './i18n-config';
+
+function getLocale(request: NextRequest): string | undefined {
+  // Negotiator expects plain object so we need to transform headers
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  // Use negotiator and intl-localematcher to get best locale
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  // @ts-ignore locales are readonly
+  const locales: string[] = i18n.locales;
+
+  return matchLocale(languages, locales, i18n.defaultLocale);
+}
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const { locales } = i18n;
-  const pathnameIsMissingLocale = locales.every(
+
+  const pathnameIsMissingLocale = i18n.locales.every(
     locale => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`,
   );
 
   if (pathnameIsMissingLocale) {
-    const locale = request.cookies.get('NEXT_LOCALE')?.value || locales[0];
+    const locale = getLocale(request);
 
     return NextResponse.redirect(new URL(`/${locale + pathname}`, request.url));
   }
