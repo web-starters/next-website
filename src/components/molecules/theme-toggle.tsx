94ab13e0f58e70da1414abbc7038b86467741f7a
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-06-27 17:49:21 +0200

    changed theme toggle

--- src/components/molecules/theme-toggle.tsx
+++ src/components/molecules/theme-toggle.tsx
@@ -1,23 +1,33 @@
 'use client';
 
-import { useCallback } from 'react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/atoms/button';
 import { Icons } from '@/components/atoms/icons';
+import { Button } from '@/components/atoms/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/molecules/dropdown-menu';
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
-
-  const toggleTheme = useCallback(() => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  }, [theme, setTheme]);
+  const { setTheme } = useTheme();
 
   return (
-    <Button variant="ghost" size="sm" onClick={toggleTheme}>
-      <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
