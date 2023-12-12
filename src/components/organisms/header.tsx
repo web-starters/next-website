3d60ff66121f23f69a18d3638eaaf007d4337ddc
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-12 11:15:01 +0100

    changed logo

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,5 +1,7 @@
 import { useTranslations } from 'next-intl';
 
+import { Link } from '@/lib/navigation';
+
 import { Icons } from '@/components/atoms/icons';
 import { LocaleToggle } from '@/components/molecules/locale-toggle';
 import { ThemeToggle } from '@/components/molecules/theme-toggle';
@@ -16,7 +18,9 @@ export function Header() {
   return (
     <header className="w-full p-5 border-b bg-palette-50 dark:bg-palette-900">
       <div className="flex justify-between items-center w-full max-w-7xl mx-auto space-x-10">
-        <Icons.logo className="w-20 h-6" />
+        <Link href="/">
+          <Icons.logo className="w-24 h-5" />
+        </Link>
 
         <Navigation items={navigationItems} />
 
